0b45ff15437defe8437433196a7441333f596ad6
Jeremiah Adeboye <91225691+jsCoder07@users.noreply.github.com>
2025-06-15 22:04:37 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -24,14 +24,17 @@ export default function Home() {
 
       <main className="flex min-h-screen flex-col items-center justify-between">
         <Hero />
-       
+      
         <Introduction eventCenterName="Capitol City Hall" />
-        
+          
+            <ScrollFadeIn>
         <ServicesOverview />
-       
+       </ScrollFadeIn>
+            
+            <ScrollFadeIn>
      <WhyChooseUs />
         {/* ... more content */}
-
+            </ScrollFadeIn>
         <UpcomingEvents/>
       </main>
 
